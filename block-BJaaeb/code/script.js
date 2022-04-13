79d1b3a7d4b17291558d7509a64d73c76bcb1fe6
Vishalgoswami12 <vishalgoswami2250@gmail.com>
2022-04-13 15:00:40 +0530

    block-BJaaeb

--- block-BJaaeb/code/script.js
+++ block-BJaaeb/code/script.js
@@ -6,7 +6,15 @@ default value to be "text" and return the input element inside label. (create it
 */
 
 // Your code goes here
-
+function createInputElm(label,type="text"){
+let label1 = document.createElement('label');
+label1.innerText=label;
+let input=document.createElement('input')
+input.type=type;
+label1.append(input);
+document.querySelector('body').append(label)
+return label1;
+}
 // TEST
 createInputElm('Your name'); //<label>Your name: <input type="text"></label>
 createInputElm('Your age', 'number'); //<label>Your age: <input type="number"></label>
@@ -14,7 +22,8 @@ createInputElm('Your age', 'number'); //<label>Your age: <input type="number"></
 // 2. Do the same thing as above using string literal like `<h1>Hello</h1>`
 
 // Your code goes here
-
+let ul=document.createElement('ul');
+ul.innerHTML='<label>Your name: <input type="text"></label>';
 // TEST
 createInputElm('Your name'); //<label>Your name: <input type="text"></label>
 createInputElm('Your age', 'number'); //<label>Your age: <input type="number"></label>
@@ -22,7 +31,15 @@ createInputElm('Your age', 'number'); //<label>Your age: <input type="number"></
 // 3. Create a function named `createList` that accept and array of data like ['Mango', 'Apple', 'Banana'] and returns
 // the html for the link like <ul> <li>Mango</li>  <li>Apple</li>  <li>Banana</li> </ul>
 // Your code goes here
-
+function createList(array){
+  let ul=document.createElement('ul');
+  array.forEach(cv=>{
+    let li = document.createElement('li');
+    li.innerText = cv;
+    ul.append(li);
+  })
+  return ul;
+}
 // TEST
 createList(['ALABAMA', 'ALASKA', 'HAWAII', 'KENTUCKY']);
 createList(['Afghanistan', 'Antarctica', 'Congo', 'Estonia']);
@@ -39,8 +56,36 @@ createList(['Afghanistan', 'Antarctica', 'Congo', 'Estonia']);
 </ul>
 */
 
-// Your code goes here
+function createTodoList(array){
+let ul = document.createElement("ul");
+let root =document.querySelector('html')
+array.forEach((ele)=>{
+  let li = document.createElement("li");
+  let p  = document.createElement("p");
+  p.innerText = ele.name;
+  let input = document.createElement("input");
+  input.checked = true;
+  input.type  = "checkbox";
+  let span = document.createElement("span");
+  span.innerText = "X";
+  li.append(p,input,span);
+  ul.append(li)
+})
+  return root.append(ul);
+}
 
+// Your code goes here
+function createTodoList(array){
+  let ul=document.createElement('ul');
+  array.forEach(cv=>{
+    let p=document.createElement('p');
+    let li = document.createElement('li');
+    p.innerText=cv
+    li.innerText = cv;
+    ul.append(li,p);
+  })
+  return ul;
+}
 // TEST
 createTodoList([
   { name: 'Learn DOM', isDone: false },
